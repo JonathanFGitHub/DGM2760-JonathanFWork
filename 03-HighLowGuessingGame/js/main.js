ec8e12cabd4fac649e32e6103fd077b56199bc6f
Jonathan Francis <jonathanfrancis@jonathans-mbp.lan>
2022-01-30 21:18:11 -0700

    Adding updates

--- 03-HighLowGuessingGame/js/main.js
+++ 03-HighLowGuessingGame/js/main.js
@@ -18,7 +18,7 @@ function evalGuess() {
     const feedback = document.querySelector('#feedback')
 
     if (gamerGuess == correctNumber) {
-        feedback.innerText = 'You are a winner!!!'
+        feedback.innerText = 'You Win!!!'
         giveAward()
     }
     else if (gamerGuess > correctNumber && gamerGuess < 16) {
@@ -31,38 +31,39 @@ function evalGuess() {
         feedback.innerText = 'Please Choose a number between 1 & 15 and try again.'
         totalGuesses -= 1
     }
-    //End of if else statement
+    //End of if-else statement
 
     const attempts = document.querySelector('#attempts')
     attempts.innerText = `${totalGuesses}`
-}// End of evalGuess() Function
+}   //End of evalGuess() Function
 
 function giveAward() {
     let imgPath = 'images/yellowribbon.png'
+    console.log("Congratulations")
     switch(totalGuesses) {
         case 1:
         case 2:
         case 3: 
             imgPath = 'images/blueribbon.png'
-            console.log('Blue Ribbon for you')
+            console.log('Blue ribbon for you')
             break;
         case 4:
         case 5:
         case 6: 
             imgPath = 'images/redribbon.png'
-            console.log('Red Ribbon for you')
+            console.log('Red ribbon for you')
             break;
         case 7:
-            console.log('Yellow Ribbon for you')
+            console.log('Yellow ribbon for you')
             break;
     }
 
-    let awardImage = document.createElement('img') //Creates a <img> element in the 
+    let awardImage = document.createElement('img') //Creating an <img> tag element in the DOM
     awardImage.setAttribute('src', imgPath)
 
     const ribbon = document.querySelector('#ribbon') //assigns ribbon to #ribbon div in DOM
     ribbon.appendChild(awardImage)
-
+    //only append child if the ribbon element does not have child nodes yet. 
     
     
 }
